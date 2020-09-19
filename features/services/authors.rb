537517d274598514665723dcc11d3c4a88b24050
Marcelo Feiteiro <mfeiteiro@hotmail.com>
2020-09-19 11:02:18 -0300

    Projeto

---
+++ features/services/authors.rb
@@ -0,0 +1,31 @@
+class Authors
+    include HTTParty
+    
+    headers 'Content-Type' => 'application/json'
+    base_uri 'http://fakerestapi.azurewebsites.net'
+
+    def get_author_by_book(id)
+        self.class.get("/authors/books/#{id}")        
+    end
+
+    def get_authors
+        self.class.get('/api/Authors')        
+    end
+
+    def post_author(author)
+        self.class.post('/api/Authors', body: author.to_json)        
+    end
+
+    def delete_authors(id)
+        self.class.delete("/api/Authors/#{id}")        
+    end
+
+    def get_authors_by_id(id)
+        self.class.get("/api/Authors/#{id}")        
+    end
+
+    def put_authors(author)
+        self.class.put("/api/Authors/#{author.id}", body: author.to_json)        
+    end
+
+end
--- features/support/services.rb
+++ features/support/services.rb
@@ -1,3 +1,11 @@
 class Services
 
+    def authors
+        Authors.new
+    end
+
+    def books
+        Books.new        
+    end
+
 end
